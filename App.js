202e07d23f3e7619edcf546370b0c5c6397bd2f0
xavierloos <xavier.loos04@gmail.com>
2021-06-04 15:50:11 +0100

    Navigation container

--- App.js
+++ App.js
@@ -1,13 +1,18 @@
+import 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>hiMate</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <View style={styles.container}>
+        <Text>hiMate</Text>
+        <StatusBar style="auto" />
+      </View>
+    </NavigationContainer>
+    
   );
 }
 
--- README.md
+++ README.md
@@ -7,3 +7,4 @@ Group chat app
 Install the react navigation: `yarn add @react-navigation/native`
 Install all dependencies to your project: `expo install react-native-gesture-handler react-native-reanimated react-native-screens react-native-safe-area-context @react-native-community/masked-view`
 Add the import to the app.js file: `import 'react-native-gesture-handler';`
+Add the navigation container to the app.js file: `import { NavigationContainer } from '@react-navigation/native';` and sorround the `<View>` with the `</NavigationContainer>` tag
