8fa31d6610ae2f6070966ea10c64dafedab6e870
xavierloos <xavier.loos04@gmail.com>
2021-06-04 15:40:48 +0100

    Firebase

---
+++ firebare.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBVBwdx1nC5jKgH2gu6w50WH5n6WlOYmz0",
+  authDomain: "himate-a26bc.firebaseapp.com",
+  projectId: "himate-a26bc",
+  storageBucket: "himate-a26bc.appspot.com",
+  messagingSenderId: "1024285819803",
+  appId: "1:1024285819803:web:7fcd537d76f880a3a3973e"
+};
