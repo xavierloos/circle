d0a3efa5ade9b183de18f161f4758681313d1560
xavierloos <xavier.loos04@gmail.com>
2021-06-09 14:04:17 +0100

    Email

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,12 +1,14 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { StyleSheet, View } from 'react-native';
-import {Button, Input, Image, Text} from "react-native-elements"
+import { Button, Input, Image, Text } from "react-native-elements"
 import { StatusBar } from "expo-status-bar";
 
 const LoginScreen = () => {
+  const [email, setEmail] = useState("")
+  const [password, setPassword] =  useState("")
   return (
-    <View style={styles.container}>
-      <StatusBar style="light"/>
+    <View>
+      <StatusBar style="light" />
       <Image
         source={{
           uri: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fst4.depositphotos.com%2F6381748%2F27917%2Fv%2F450%2Fdepositphotos_279171494-stock-illustration-hand-wave-waving-hi-or.jpg&f=1&nofb=1"
@@ -14,6 +16,9 @@ const LoginScreen = () => {
         style={styles.logo}
       />
       <Text h2>Mate</Text>
+      <View style={styles.inputContainer}>
+        <Input type="email" placeholder="Email" autoFocus/>
+      </View>
     </View>
   )
 }
@@ -21,7 +26,7 @@ const LoginScreen = () => {
 export default LoginScreen
 
 const styles = StyleSheet.create({
-  container:
+  container: {
     justifyContent: 'center',
     alignItems: 'center',
   },
@@ -29,4 +34,7 @@ const styles = StyleSheet.create({
     width: 200,
     height: 200,
   },
+  inputContainer: {
+    color: "red"
+  }
 })
