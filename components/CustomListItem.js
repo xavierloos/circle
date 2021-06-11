828a8063060fb10ef74e383c7685a34300ab7667
xavierloos <xavier.loos04@gmail.com>
2021-06-11 10:29:22 +0100

    Autoformat

--- components/CustomListItem.js
+++ components/CustomListItem.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
-import {ListItem, Avatar} from "react-native-elements"
+import { ListItem, Avatar } from "react-native-elements"
 
 const CustomListItem = () => {
   return (
     <ListItem>
-      <Avatar rounded source={{uri:"https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.9LeJajtN75YM4qNKou-0ewHaHa%26pid%3DApi&f=1"}} />
+      <Avatar rounded source={{ uri: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.9LeJajtN75YM4qNKou-0ewHaHa%26pid%3DApi&f=1" }} />
     </ListItem>
   )
 }
