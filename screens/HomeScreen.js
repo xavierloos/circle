9d77f9e84c94738cf679dccb63368f0bf8b7d5b7
xavierloos <xavier.loos04@gmail.com>
2021-06-11 10:21:34 +0100

    Scrolldown

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import { StyleSheet, Text, View, SafeAreaView } from 'react-native'
+import { ScrollView } from 'react-native-gesture-handler'
 
 const HomeScreen = () => {
 
   return (
     <SafeAreaView>
       <Text>Homepage Screen</Text>
+      <ScrollView>
+
+      </ScrollView>
     </SafeAreaView>
   )
 
