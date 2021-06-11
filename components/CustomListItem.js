57fbdf943e7df0a11b7312ea3603092b3f86fc55
xavierloos <xavier.loos04@gmail.com>
2021-06-11 10:24:33 +0100

    Components

---
+++ components/CustomListItem.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const CustomListItem = () => {
+  return (
+    <View>
+      <Text></Text>
+    </View>
+  )
+}
+
+export default CustomListItem
+
+const styles = StyleSheet.create({})
