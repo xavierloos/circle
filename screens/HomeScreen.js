454d773b7a92917f8227e075628e47dc2c960839
xavierloos <xavier.loos04@gmail.com>
2021-06-11 11:40:10 +0100

    Header properties style

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,13 +1,25 @@
-import React from 'react'
+import React, { useLayoutEffect } from 'react'
 import { ScrollView, StyleSheet, Text, View, SafeAreaView } from 'react-native'
 import CustomListItem from '../components/CustomListItem'
+import { NavigationContainer } from '@react-navigation/native'
+import { StatusBar } from 'expo-status-bar'
 
-const HomeScreen = () => {
+const HomeScreen = ({ navigation }) => {
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      title: "hiMate Chat",
+      headerStyle: { backgroundColor: "#D50000" },
+      headerTitleStyle: { color: "white" },
+      headerTintColor: "black"
+    })
+  }, [])
 
   return (
+
     <SafeAreaView>
+      <StatusBar style="light" />
       <ScrollView>
-        <CustomListItem/>
+        <CustomListItem />
       </ScrollView>
     </SafeAreaView>
   )
