f2235d8bef0a6c6e82e1b8615d4e5423b3b15652
xavierloos <xavier.loos04@gmail.com>
2021-06-11 10:28:37 +0100

    Default avatar

--- components/CustomListItem.js
+++ components/CustomListItem.js
@@ -5,7 +5,7 @@ import {ListItem, Avatar} from "react-native-elements"
 const CustomListItem = () => {
   return (
     <ListItem>
-      <Text></Text>
+      <Avatar rounded source={{uri:"https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.9LeJajtN75YM4qNKou-0ewHaHa%26pid%3DApi&f=1"}} />
     </ListItem>
   )
 }
