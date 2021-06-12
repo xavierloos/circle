57fc3bff8b3aab330dedfc06d71de247e1041288
xavierloos <xavier.loos04@gmail.com>
2021-06-12 13:03:53 +0100

    Add chat stack

--- App.js
+++ App.js
@@ -25,6 +25,7 @@ export default function App() {
         <Stack.Screen name="Login" component={LoginScreen} />
         <Stack.Screen name="Register" component={RegisterScreen} />
         <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="AddChat" component={AddChatScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
