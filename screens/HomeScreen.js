73103ac2056a9afacca3f6e2d72c8cacfdc3dcc6
xavierloos <xavier.loos04@gmail.com>
2021-06-12 11:37:36 +0100

    Profile picture working

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,22 +1,23 @@
 import React, { useLayoutEffect } from 'react'
-import { ScrollView, StyleSheet, Text, View, SafeAreaView, TouchableOpacity} from 'react-native'
+import { ScrollView, StyleSheet, Text, View, SafeAreaView, TouchableOpacity } from 'react-native'
 import CustomListItem from '../components/CustomListItem'
-import { NavigationContainer } from '@react-navigation/native'
 import { StatusBar } from 'expo-status-bar'
-import { Avatar } from 'react-native-elements/dist/avatar/Avatar'
+import { Avatar } from 'react-native-elements'
 import { auth, db } from '../firebase'
 
 const HomeScreen = ({ navigation }) => {
+  console.log(auth?.currentUser?.email)
+  console.log(auth?.currentUser?.photoURL)
   useLayoutEffect(() => {
     navigation.setOptions({
       title: "hiMate Chat",
       headerStyle: { backgroundColor: "#D50000" },
       headerTitleStyle: { color: "white" },
       headerTintColor: "black",
-      headerLeft: () => (
+      headerRight: () => (
         <View style={{ marginRight: 20 }}>
           <TouchableOpacity>
-            <Avatar rounded source={{ uri: auth?.currentUser?.photoURL }} />
+            <Avatar rounded source={{ uri: "http://icons.iconarchive.com/icons/pelfusion/long-shadow-media/512/Contact-icon.png" }} />
           </TouchableOpacity>
         </View>
       ),
