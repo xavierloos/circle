36a3a8e0b0095ea645f36b9812abb53c35bbcb78
xavierloos <xavier.loos04@gmail.com>
2021-06-12 13:09:27 +0100

    Add a new chat title

--- screens/AddChatScreen.js
+++ screens/AddChatScreen.js
@@ -2,9 +2,10 @@ import React, { useLayoutEffect } from 'react'
 import { StyleSheet, Text, View } from 'react-native'
 
 const AddChatScreen = ({ navigation }) => {
-  
+
   useLayoutEffect(() => {
     navigation.setOptions({
+      title: "Add a new chat",
       headerBackTitle: "Cancel",
     });
   }, [navigation])
