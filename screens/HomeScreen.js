797eb4c3a8300832e39b58bf65c2f8c4017af373
xavierloos <xavier.loos04@gmail.com>
2021-06-12 12:02:20 +0100

    Camara icon

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -16,8 +16,14 @@ const HomeScreen = ({ navigation }) => {
       headerTitleStyle: { color: "white" },
       headerTintColor: "black",
       headerRight: () => (
-        <View style={{ marginRight: 20 }}>
-          <TouchableOpacity>
+        <View style={{ marginRight: 10, flexDirection:"row", justifyContent:"space-between",alignItems: "center" }}>
+          <TouchableOpacity activeOpacity={0.5} style={{ marginLeft: 5 }}>
+            <AntDesign name="camera" size={24} color="white"/>
+          </TouchableOpacity>
+          <TouchableOpacity activeOpacity={0.5} style={{ marginLeft: 5 }}>
+            <Avatar rounded source={{ uri: "http://icons.iconarchive.com/icons/pelfusion/long-shadow-media/512/Contact-icon.png" }} />
+          </TouchableOpacity>
+          <TouchableOpacity activeOpacity={0.5} style={{ marginLeft: 5 }}>
             <Avatar rounded source={{ uri: "http://icons.iconarchive.com/icons/pelfusion/long-shadow-media/512/Contact-icon.png" }} />
           </TouchableOpacity>
         </View>
