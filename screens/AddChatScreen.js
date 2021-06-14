b4d7af44c28216bd4d3f0df071da340cc53eb2d4
xavierloos <xavier.loos04@gmail.com>
2021-06-14 21:13:54 +0100

    chat name input

--- screens/AddChatScreen.js
+++ screens/AddChatScreen.js
@@ -1,7 +1,9 @@
-import React, { useLayoutEffect } from 'react'
+import React, { useLayoutEffect, useState } from 'react'
 import { StyleSheet, Text, View } from 'react-native'
+import { Input } from 'react-native-elements';
 
 const AddChatScreen = ({ navigation }) => {
+  const [chatname,setChatname] = useState("")
   useLayoutEffect(() => {
     navigation.setOptions({
       title: "Add a new chat",
@@ -12,6 +14,7 @@ const AddChatScreen = ({ navigation }) => {
   return (
     <View>
       <Text>Add Chat Screen</Text>
+      <Input placeholder="Chat name" value={chatname} onChangeText={() => setChatname(chatname)} />
     </View>
   )
 }
