8c9cc0867df6d13a53af84cfcf31dd36b0ce7380
xavierloos <xavier.loos04@gmail.com>
2021-06-14 21:28:33 +0100

    Connect to firebase to add new chat

--- screens/AddChatScreen.js
+++ screens/AddChatScreen.js
@@ -1,8 +1,9 @@
 import React, { useLayoutEffect, useState } from 'react'
 import { StyleSheet, Text, View } from 'react-native'
-import { Input,Icon, Button } from 'react-native-elements';
+import { Input, Icon, Button } from 'react-native-elements';
+import { db } from '../firebase';
 const AddChatScreen = ({ navigation }) => {
-  const [chatname,setChatname] = useState("")
+  const [chatname, setChatname] = useState("")
   useLayoutEffect(() => {
     navigation.setOptions({
       title: "Add a new chat",
@@ -10,15 +11,20 @@ const AddChatScreen = ({ navigation }) => {
     });
   }, [navigation])
 
-  const createChat=()=>{}
+  const createChat = async() => {
+    await db.collection("chats").add({
+      chatName: chatname,
+    })
+      
+  }
 
   return (
     <View>
       <Text>Add Chat Screen</Text>
       <Input placeholder="Chat name" value={chatname} onChangeText={() => setChatname(chatname)} leftIcon={
-        <Icon name="wechat" type="antdesign" size={30} color="black"/>
+        <Icon name="wechat" type="antdesign" size={30} color="black" />
       } />
-      <Button onPress={createChat} title="Create chat"/>
+      <Button onPress={createChat} title="Create chat" />
     </View>
   )
 }
