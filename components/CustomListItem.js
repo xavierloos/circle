04b7f6a95a5f3387b47138cecc045a044fbabbd3
xavierloos <xavier.loos04@gmail.com>
2021-06-16 16:24:58 +0100

    Enter chat

--- components/CustomListItem.js
+++ components/CustomListItem.js
@@ -4,7 +4,7 @@ import { ListItem, Avatar } from "react-native-elements"
 
 const CustomListItem = ({id, chatName, enterChat}) => {
   return (
-    <ListItem key={id} bottomDivider>
+    <ListItem onPress={()=>enterChat(id,chatName)} key={id} bottomDivider>
       <Avatar rounded source={{ uri: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.9LeJajtN75YM4qNKou-0ewHaHa%26pid%3DApi&f=1" }} />
       <ListItem.Content>
         <ListItem.Title style={{ fontWeight: "600" }}>{chatName}</ListItem.Title>
