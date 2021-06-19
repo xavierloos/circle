108afc33b2cb901c9ce26e9d27e2dca3fc420b34
xavierloos <xavier.loos04@gmail.com>
2021-06-19 10:49:19 +0100

    Chat information screen

---
+++ screens/ChatInfoScreen.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const ChatInfoScreen = () => {
+  return (
+    <View>
+      <Text>Chat information</Text>
+    </View>
+  )
+}
+
+export default ChatInfoScreen
+
+const styles = StyleSheet.create({})
