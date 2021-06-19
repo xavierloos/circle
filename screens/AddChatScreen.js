ec13ebad519b864dff649b51fb1e7a0e4244b71c
xavierloos <xavier.loos04@gmail.com>
2021-06-19 10:47:42 +0100

    Delete unused elements

--- screens/AddChatScreen.js
+++ screens/AddChatScreen.js
@@ -1,5 +1,5 @@
 import React, { useLayoutEffect, useState } from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, View } from 'react-native'
 import { Input, Icon, Button } from 'react-native-elements';
 import { db } from '../firebase';
 const AddChatScreen = ({ navigation }) => {
