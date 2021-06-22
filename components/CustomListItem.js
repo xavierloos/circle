16b56a9505384ed06c75c1ec42d3773b452e4dba
xavierloos <xavier.loos04@gmail.com>
2021-06-22 21:48:26 +0100

    Update

--- components/CustomListItem.js
+++ components/CustomListItem.js
@@ -21,7 +21,7 @@ const CustomListItem = ({ id, chatName, enterChat }) => {
   // const messageInfo = `${lastMessage?.[0]?.displayName}: ${lastMessage?.[0]?.message}`
 
   return (
-    <ListItem key={id} onPress={() => enterChat(id, chatName)} key={id} bottomDivider>
+    <ListItem onPress={() => enterChat(id, chatName)} key={id} bottomDivider>
       <Avatar rounded source={{ uri: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.9LeJajtN75YM4qNKou-0ewHaHa%26pid%3DApi&f=1" }} />
       <ListItem.Content>
         <ListItem.Title style={{ fontWeight: "600" }}>{chatName}</ListItem.Title>
