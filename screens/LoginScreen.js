841e44d4eb9fd3627031545106d70433b4bbd9b2
xavierloos <xavier.loos04@gmail.com>
2021-06-22 22:13:20 +0100

    Update login text

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -34,13 +34,7 @@ const LoginScreen = ({ navigation }) => {
   return (
     <KeyboardAvoidingView behavior="padding" style={styles.container}>
       <StatusBar style="light" />
-      <Image
-        source={{
-          uri: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fst4.depositphotos.com%2F6381748%2F27917%2Fv%2F450%2Fdepositphotos_279171494-stock-illustration-hand-wave-waving-hi-or.jpg&f=1&nofb=1"
-        }}
-        style={styles.logo}
-      />
-      <Text h2>Mate</Text>
+      <Text h2>Login</Text>
       <View style={styles.inputContainer}>
         <Input type="email" placeholder="Email" autoFocus value={email} onChangeText={(text) => setEmail(text)} />
         <Input type="password" placeholder="Password" value={password} onChangeText={(text) => setPassword(text)} onSubmitEditing={singIn} />
