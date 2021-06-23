2c35d9f36465fd8bd91df49d72498b4869c6cfdb
xavierloos <xavier.loos04@gmail.com>
2021-06-23 16:26:53 +0100

    Require database from firebase

--- components/CustomListItem.js
+++ components/CustomListItem.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import { StyleSheet } from 'react-native'
 import { ListItem, Avatar } from "react-native-elements"
-// import { db } from "../firebase"
+import { db } from "../firebase"
 
 const CustomListItem = ({ id, chatName, enterChat }) => {
   const [lastMessage, setLastMessage] = useState([])
