63bf165b2f41650710edc921c52d09b28fbd9a1e
xavierloos <xavier.loos04@gmail.com>
2021-06-23 16:33:20 +0100

    name

--- screens/RegisterScreen.js
+++ screens/RegisterScreen.js
@@ -13,7 +13,7 @@ const RegisterScreen = ({ navigation }) => {
 
   useLayoutEffect(() => {
     navigation.setOptions({
-      headerTitle: "ЯƎD",
+      headerTitle: "CIRCLE",
       headerStyle: { backgroundColor: "#D50000" },
       headerTitleStyle: { color: "white" },
       headerTintColor: "white",
