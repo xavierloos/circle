601bf8fcd360d5f68844c924b5fec1a0f9a3931a
xavierloos <xavier.loos04@gmail.com>
2021-06-24 15:08:38 +0100

    User icon in profile screen

--- screens/ProfileScreen.js
+++ screens/ProfileScreen.js
@@ -1,6 +1,7 @@
 import React, { useLayoutEffect } from 'react'
 import { StyleSheet, Text, View } from 'react-native'
 import { Avatar } from 'react-native-elements'
+import { SimpleLineIcons } from "@expo/vector-icons"
 import { auth, db } from '../firebase'
 
 const ProfileScreen = ({ navigation }) => {
@@ -17,7 +18,10 @@ const ProfileScreen = ({ navigation }) => {
     <View style={styles.container}>
       {console.log(auth?.currentUser)}
       <Avatar size={100} rounded source={{ uri: auth?.currentUser?.photoURL }} />
-      <Text style={styles.text}>{auth?.currentUser?.displayName}</Text>
+      <View style={styles.infoContainer}>
+        <SimpleLineIcons name="user" size={20} color="white" style={{ marginRight: 5 }} /><Text style={styles.text}>{auth?.currentUser?.displayName}</Text>
+      </View>
+
       <Text style={styles.text}>{auth?.currentUser?.email}</Text>
     </View>
   )
@@ -33,9 +37,15 @@ const styles = StyleSheet.create({
     padding: 10,
     backgroundColor: "white"
   },
+  infoContainer: {
+    display: "flex",
+    flexDirection: "row",
+    alignItems: 'baseline',
+    justifyContent: 'center',
+  },
   text: {
     fontWeight: "600",
-    fontSize: 16,
+    fontSize: 18,
     marginTop: 20
   },
 })
