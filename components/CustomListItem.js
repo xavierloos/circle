db3cb9085cac84a0466719b4aa3d8b6d726309f5
xavierloos <xavier.loos04@gmail.com>
2021-06-26 15:16:50 +0100

    uPDATE

--- components/CustomListItem.js
+++ components/CustomListItem.js
@@ -30,25 +30,32 @@ const CustomListItem = () => {
     //     </ListItem.Subtitle>
     //   </ListItem.Content>
     // </ListItem>
-    <View></View>
-    <ListItem bottomDivider style={styles.itemConteiner}>
-      <Avatar rounded source={{ uri: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.9LeJajtN75YM4qNKou-0ewHaHa%26pid%3DApi&f=1" }} />
-      <ListItem.Content>
-        <ListItem.Title style={{ fontWeight: "600" }}>hello</ListItem.Title>
-        <ListItem.Subtitle numberOfLines={1} ellipsizeMode="tail" style={{ fontWeight: "400" }}>
-          description
+    <View style={styles.itemsConteiner}>
+      <ListItem bottomDivider style={styles.item}>
+        <Avatar rounded source={{ uri: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.9LeJajtN75YM4qNKou-0ewHaHa%26pid%3DApi&f=1" }} />
+        <ListItem.Content>
+          <ListItem.Title style={{ fontWeight: "600" }}>hello</ListItem.Title>
+          <ListItem.Subtitle numberOfLines={1} ellipsizeMode="tail" style={{ fontWeight: "400" }}>
+            description
       </ListItem.Subtitle>
-      </ListItem.Content>
-    </ListItem>
+        </ListItem.Content>
+      </ListItem>
+    </View>
+
   )
 }
 export default CustomListItem
 const styles = StyleSheet.create({
-  itemConteiner: {
+  itemsContainer: {
     display: "flex",
     flexDirection: "row",
-    justifyContent:"space-evenly",
+    justifyContent: "space-evenly",
+    alignItems: "flex-start",
     width: "50%"
+
+  },
+  item: {
+
 
   }
 })
