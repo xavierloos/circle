030e4c4b3723d1f295b0f28eac04f7b859f44645
xavierloos <xavier.loos04@gmail.com>
2021-06-27 20:15:54 +0100

    Comment

--- components/CustomListItem.js
+++ components/CustomListItem.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import { StyleSheet, Text, View } from 'react-native'
 import { ListItem, Avatar } from "react-native-elements"
-import { db } from "../firebase"
+// import { db } from "../firebase"
 // const CustomListItem = ({ id, chatName, enterChat }) => {
 const CustomListItem = () => {
   const [lastMessage, setLastMessage] = useState([])
