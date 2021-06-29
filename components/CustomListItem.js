af46fe0b33a4f9fc4c229f14489f5e36b9688b3a
xavierloos <xavier.loos04@gmail.com>
2021-06-29 16:04:59 +0100

    circle style update

--- components/CustomListItem.js
+++ components/CustomListItem.js
@@ -1,8 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { StyleSheet, View, Touchable } from 'react-native'
-import { ListItem, Avatar, Card, Image, Text, Button } from "react-native-elements"
-import TouchableScale from 'react-native-touchable-scale'
-import { color } from 'react-native-reanimated'
+import { ListItem, Avatar, Card, Image, Text, Button, Icon } from "react-native-elements"
 import { TouchableOpacity } from 'react-native-gesture-handler'
 import { db } from "../firebase"
 
@@ -22,64 +20,15 @@ const CustomListItem = ({ id, chatName, enterChat }) => {
 
   const messageInfo = `${lastMessage?.[0]?.displayName}: ${lastMessage?.[0]?.message}`
   return (
-    // <ListItem key={id} onPress={() => enterChat(id, chatName)} key={id} bottomDivider>
-    //   <Avatar rounded source={{ uri: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.9LeJajtN75YM4qNKou-0ewHaHa%26pid%3DApi&f=1" }} />
-    //   <ListItem.Content>
-    //     <ListItem.Title style={{ fontWeight: "600" }}>{chatName}</ListItem.Title>
-    //     <ListItem.Subtitle numberOfLines={1} ellipsizeMode="tail" style={{ fontWeight: "400" }}>
-    //       {(typeof lastMessage[0]?.displayName === 'undefined') ? 'No messages yet' : "messageInfo"}
-    //     </ListItem.Subtitle>
-    //   </ListItem.Content>
-    // </ListItem>
     <View style={styles.itemsContainer}>
-      {/* <TouchableOpacity activeOpacity={0.5} style={{ marginLeft: 5 }} onPress={() => navigation.navigate("Profile")}> */}
       <TouchableOpacity activeOpacity={0.5} style={styles.cardTouchable} key={id} onPress={() => enterChat(id, chatName)}>
         <View style={styles.card}>
-          <Image
-            style={styles.image}
-            resizeMode="cover"
-            source={{ uri: 'https://miro.medium.com/max/1178/1*rishAJIUgRCz_VzJV-vZuA.png' }}
-          />
+          <Image style={styles.image} source={{ uri: 'https://miro.medium.com/max/1178/1*rishAJIUgRCz_VzJV-vZuA.png' }} />
           <Text h5 style={styles.title} numberOfLines={1}>{chatName}</Text>
           <Text h6 style={styles.description} numberOfLines={2}>Long long description long description long description long long description</Text>
           <Text h6 style={styles.enter} numberOfLines={1}>Chat now</Text>
         </View>
       </TouchableOpacity>
-
-
-      {/* <ListItem
-        Component={TouchableScale}
-        friction={90} //
-        tension={100} // These props are passed to the parent component (here TouchableScale)
-        activeScale={0.95} //
-        style={styles.item}>
-        <Avatar rounded source={{ uri: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.9LeJajtN75YM4qNKou-0ewHaHa%26pid%3DApi&f=1" }} />
-        <ListItem.Content>
-          <ListItem.Title style={{ fontWeight: "600" }}>hello</ListItem.Title>
-          <ListItem.Subtitle numberOfLines={1} ellipsizeMode="tail" style={{ fontWeight: "400" }}>
-            description
-      </ListItem.Subtitle>
-        </ListItem.Content>
-        <Button title="Join" />
-      </ListItem>
-      <ListItem style={styles.item}>
-        <Avatar rounded source={{ uri: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.9LeJajtN75YM4qNKou-0ewHaHa%26pid%3DApi&f=1" }} containerStyle={styles.avatar} />
-        <ListItem.Content>
-          <ListItem.Title style={{ fontWeight: "600" }}>hello</ListItem.Title>
-          <ListItem.Subtitle numberOfLines={1} ellipsizeMode="tail" style={{ fontWeight: "400" }}>
-            description with long description
-      </ListItem.Subtitle>
-        </ListItem.Content>
-      </ListItem>
-      <ListItem style={styles.item}>
-        <Avatar rounded source={{ uri: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.9LeJajtN75YM4qNKou-0ewHaHa%26pid%3DApi&f=1" }} containerStyle={styles.avatar} />
-        <ListItem.Content>
-          <ListItem.Title style={{ fontWeight: "600" }}>hello</ListItem.Title>
-          <ListItem.Subtitle numberOfLines={1} ellipsizeMode="tail" style={{ fontWeight: "400" }}>
-            description with long description
-      </ListItem.Subtitle>
-        </ListItem.Content>
-      </ListItem> */}
     </View>
   )
 }
