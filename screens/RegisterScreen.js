7d9a6c2a2b17e1b6a81ff85a1ed696777be9826e
xavierloos <xavier.loos04@gmail.com>
2021-06-30 13:17:29 +0100

    Scroll on register

--- screens/RegisterScreen.js
+++ screens/RegisterScreen.js
@@ -1,12 +1,12 @@
 import React, { useState, useLayoutEffect } from 'react'
-import { View, KeyboardAvoidingView, StyleSheet } from 'react-native'
+import { View, KeyboardAvoidingView, StyleSheet, ScrollView } from 'react-native'
 import { Button, Input, Text, Icon } from "react-native-elements"
 import { StatusBar } from 'expo-status-bar'
 import { auth } from "../firebase"
 import { color } from 'react-native-reanimated'
 const RegisterScreen = ({ navigation }) => {
   const [firstName, setFirstName] = useState("")
-  const [surname, setSuname] = useState("")
+  const [surname, setSurname] = useState("")
   const [email, setEmail] = useState("")
   const [password, setPassword] = useState("")
   const [imageUrl, setImageUrl] = useState("")
@@ -33,24 +33,31 @@ const RegisterScreen = ({ navigation }) => {
       .catch((error) => alert(error.message))
   }
   return (
-    <KeyboardAvoidingView behavior="padding" style={styles.container}>
-      <StatusBar style="light" />
-      <Text h2 style={styles.title}>Register</Text>
-      <View style={styles.inputContainer}>
-        <Input style={styles.input} type="text" placeholder="First name" autoFocus value={firstName} onChangeText={(text) => setFirstName(text)} leftIcon={<Icon name="user" type="font-awesome" size={30} color="#D50000" style={{marginRight:10}}/>} />
-        <Input type="text" placeholder="Surname" autoFocus value={surname} onChangeText={(text) => setSurname(text)} leftIcon={<Icon name="id-card" type="font-awesome" size={30} color="#D50000" style={{marginRight:10}}/>}/>
-        <Input type="email" placeholder="Email" value={email} onChangeText={(text) => setEmail(text)} leftIcon={<Icon name="at" type="font-awesome" size={30} color="#D50000" style={{marginRight:10}}/>}/>
-        {/* secureTextEntry */}
-        <Input type="password" placeholder="Password" value={password} onChangeText={(text) => setPassword(text)} leftIcon={<Icon name="lock" type="font-awesome" size={30} color="#D50000" style={{marginRight:10}} />}/>
-        <Input type="text" placeholder="Profile picture URL" value={imageUrl} onChangeText={(text) => setImageUrl(text)} onSubmitEditing={register} leftIcon={<Icon name="photo" type="font-awesome" size={30} color="#D50000" style={{marginRight:10}} />}/>
-      </View>
-      <Button raised disabled={!password} containerStyle={styles.button} raised onPress={register} title="Register" />
-      <View style={{ height: 100 }} />
-    </KeyboardAvoidingView>
+    <ScrollView style={styles.scroll}>
+      <KeyboardAvoidingView behavior="padding" style={styles.container}>
+        <StatusBar style="light" />
+        <Text h2 style={styles.title}>Register</Text>
+        <View style={styles.inputContainer}>
+          <Input style={styles.input} type="text" placeholder="First name" autoFocus value={firstName} onChangeText={(text) => setFirstName(text)} leftIcon={<Icon name="user" type="font-awesome" size={30} color="#D50000" style={{ marginRight: 10 }} />} />
+          <Input type="text" placeholder="Surname" autoFocus value={surname} onChangeText={(text) => setSurname(text)} leftIcon={<Icon name="id-card" type="font-awesome" size={30} color="#D50000" style={{ marginRight: 10 }} />} />
+          <Input type="email" placeholder="Email" value={email} onChangeText={(text) => setEmail(text)} leftIcon={<Icon name="at" type="font-awesome" size={30} color="#D50000" style={{ marginRight: 10 }} />} />
+          {/* secureTextEntry */}
+          <Input type="password" placeholder="Password" value={password} onChangeText={(text) => setPassword(text)} leftIcon={<Icon name="lock" type="font-awesome" size={30} color="#D50000" style={{ marginRight: 10 }} />} />
+          <Input type="text" placeholder="Profile picture URL" value={imageUrl} onChangeText={(text) => setImageUrl(text)} onSubmitEditing={register} leftIcon={<Icon name="photo" type="font-awesome" size={30} color="#D50000" style={{ marginRight: 10 }} />} />
+        </View>
+        <Button raised disabled={!password} containerStyle={styles.button} raised onPress={register} title="Register" />
+        <View style={{ height: 100 }} />
+      </KeyboardAvoidingView>
+    </ScrollView>
+
   )
 }
 export default RegisterScreen;
 const styles = StyleSheet.create({
+  scroll: {
+    backgroundColor: "white",
+    paddingTop:100
+  },
   container: {
     flex: 1,
     padding: 10,
