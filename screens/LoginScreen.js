e7f0b7b64e1f1e3c61efcc6b3556d79f9953b56b
xavierloos <xavier.loos04@gmail.com>
2021-06-30 12:50:09 +0100

    Login password icon

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -37,7 +37,7 @@ const LoginScreen = ({ navigation }) => {
       <Text h2 style={styles.title}>Login</Text>
       <View style={styles.inputContainer}>
         <Input type="email" placeholder="Email" autoFocus value={email} onChangeText={(text) => setEmail(text)} leftIcon={<Icon name="at" type="font-awesome" size={30} color="#D50000" />} />
-        <Input type="password" placeholder="Password" value={password} onChangeText={(text) => setPassword(text)} onSubmitEditing={singIn} />
+        <Input type="password" placeholder="Password" value={password} onChangeText={(text) => setPassword(text)} onSubmitEditing={singIn} leftIcon={<Icon name="lock" type="font-awesome" size={30} color="#D50000" />} />
       </View>
       <Button disabled={!password} title="Login" containerStyle={styles.button} onPress={singIn} />
       <Button title="Register" type="outline" containerStyle={styles.button} onPress={() => navigation.navigate("Register")} />
