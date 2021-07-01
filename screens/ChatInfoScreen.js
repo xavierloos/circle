471ad65e3f69dcb867e072ecc2ef439d192693fc
xavierloos <xavier.loos04@gmail.com>
2021-07-01 17:21:03 +0100

    Chat info use layout effect

--- screens/ChatInfoScreen.js
+++ screens/ChatInfoScreen.js
@@ -1,7 +1,15 @@
-import React from 'react'
+import React, { useLayoutEffect } from 'react'
 import { StyleSheet, Text, View } from 'react-native'
 
-const ChatInfoScreen = ({navigation}) => {
+const ChatInfoScreen = ({ navigation }) => {
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      title: "CIRCLE: Info",
+      headerStyle: { backgroundColor: "#D50000" },
+      headerTitleStyle: { color: "white" },
+      headerTintColor: "white",
+    })
+  }, [])
   return (
     <View>
       <Text>Chat information</Text>
