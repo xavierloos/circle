df7e10831a4eed7c387541fcf346ca1eeca4fe14
xavierloos <xavier.loos04@gmail.com>
2021-07-01 21:52:45 +0100

    update

--- screens/ChatInfoScreen.js
+++ screens/ChatInfoScreen.js
@@ -1,19 +1,25 @@
 import React, { useLayoutEffect } from 'react'
 import { StyleSheet, Text, View } from 'react-native'
+import { Icon } from 'react-native-elements'
 import { auth, db } from '../firebase'
 
-const ChatInfoScreen = ({ navigation }) => {
+const ChatInfoScreen = ({ navigation, route }) => {
   useLayoutEffect(() => {
     navigation.setOptions({
       title: "CIRCLE: Info",
       headerStyle: { backgroundColor: "#D50000" },
       headerTitleStyle: { color: "white" },
       headerTintColor: "white",
-    }) 
+    })
   }, [])
   return (
     <View>
-      <Text>Chat information</Text>
+      <View>
+        <Icon style={styles.icon}
+          name='user'
+          type='font-awesome'
+          color='#D50000' /><Text style={styles.text}>{route.params.chatName}</Text>
+      </View>
     </View>
   )
 }
--- screens/ChatScreen.js
+++ screens/ChatScreen.js
@@ -57,6 +57,7 @@ const ChatScreen = ({ navigation, route }) => {
           }))))
     return unsubscribe
   }, [route])
+  
   return (
     <SafeAreaView >
       <KeyboardAvoidingView behavior={Platform.OS === "ios" ? "padding" : "height"} style={styles.container} keyboardVerticalOffset={90} >
