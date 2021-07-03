2a118acbab60ba56776f698f8ec747e3a6e55e8f
xavierloos <xavier.loos04@gmail.com>
2021-07-03 15:01:57 +0100

    Import facebook

--- screens/ProfileScreen.js
+++ screens/ProfileScreen.js
@@ -1,6 +1,7 @@
 import React, { useLayoutEffect } from 'react'
 import { StyleSheet, Text, View } from 'react-native'
 import { Avatar, Icon, Button } from 'react-native-elements'
+import { Facebook } from "expo";
 // import { SimpleLineIcons } from "@expo/vector-icons"
 import { auth, db } from '../firebase'
 const ProfileScreen = ({ navigation }) => {
