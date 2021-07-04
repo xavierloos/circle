b0ae6f0e32fb73537d137819fe4eec11a18733a2
xavierloos <xavier.loos04@gmail.com>
2021-07-04 10:59:31 +0100

    Web Browser

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -12,7 +12,7 @@ export default class LoginScreen extends React.Component {
     const {
       type,
       token
-    } = await Expo.Facebook
+    } = await WebBrowser.openBrowserAsync('https://expo.io')
     if (type === "success") {
       // Get the user's name using Facebook's Graph API
       const response = await fetch(`https://graph.facebook.com/me?access_token=${token}`);
