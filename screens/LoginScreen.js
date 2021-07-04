366f7cdd8ff0771d6fee58c02040a88eb384474a
xavierloos <xavier.loos04@gmail.com>
2021-07-04 11:06:38 +0100

    Update

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -14,7 +14,6 @@ export default class LoginScreen extends React.Component {
       token
     } = await WebBrowser.openBrowserAsync(`https://graph.facebook.com/me?access_token=165654778951149`)
     if (type === "success") {
-      // Get the user's name using Facebook's Graph API
       const response = await fetch(`https://graph.facebook.com/me?access_token=${token}`);
       Alert.alert("Logged in!", `Hi ${(await response.json()).name}!`);
     }
