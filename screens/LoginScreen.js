3b2cfc6007522216fd49268903e0f0a4dd89a1f9
xavierloos <xavier.loos04@gmail.com>
2021-07-07 14:52:01 +0100

    Getting the email and full name

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -35,12 +35,15 @@ const LoginScreen = ({ navigation }) => {
   const loginFacebook = async () => {
     try {
       await Facebook.initializeAsync({ appId: '165654778951149', });
-      const { type, token } = await Facebook.logInWithReadPermissionsAsync({ permissions: ['public_profile'] });
+      const { type, token } = await Facebook.logInWithReadPermissionsAsync({ permissions: ['public_profile', "email"] });
       if (type === 'success') {
         // Get the user's name using Facebook's Graph API
-        const response = await fetch(`https://graph.facebook.com/me?access_token=${token}`);
+        const response = await fetch(`https://graph.facebook.com/v2.5/me?fields=email,first_name,last_name,friends&access_token=${token}`);
+        const body = await response.json();
+        console.log(body)
         if (response) { navigation.replace("Home") }
         console.log('Logged in!', `Hi ${(await response.json()).name}!`);
+        console.log(response.json().email)
       }
     } catch ({ message }) {
       console.log(`Facebook Login Error: ${message}`);
