ea61c4fec78b81ef3193fe0c6ff6d20aa5c6ee20
xavierloos <xavier.loos04@gmail.com>
2021-07-10 12:24:07 +0100

    Secure Text on password

--- screens/ProfileScreen.js
+++ screens/ProfileScreen.js
@@ -91,8 +91,8 @@ const ProfileScreen = ({ navigation }) => {
 
           </View>
           <Input style={styles.input} type="email" placeholder={auth?.currentUser?.email} autoFocus autoCapitalize="none" leftIcon={<Icon name="at" type="font-awesome" size={30} color="#D50000" />} />
-          <Input style={styles.input} type="email" placeholder="Password" autoCapitalize="none" leftIcon={<Icon name="lock" type="font-awesome" size={30} color="#D50000" />} />
-          <Input style={styles.input} type="email" placeholder="New Password" autoCapitalize="none" leftIcon={<Icon name="star" type="font-awesome" size={30} color="#D50000" />} />
+          <Input secureTextEntry style={styles.input} type="email" placeholder="Password" autoCapitalize="none" leftIcon={<Icon name="lock" type="font-awesome" size={30} color="#D50000" />} />
+          <Input secureTextEntry style={styles.input} type="email" placeholder="New Password" autoCapitalize="none" leftIcon={<Icon name="star" type="font-awesome" size={30} color="#D50000" />} />
         </View>
       </ModalPoup>
       <Button raised title="Logout" type="outline" containerStyle={styles.button} onPress={logout} />
