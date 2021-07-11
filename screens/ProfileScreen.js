a03a4e8cf075e4a1e884071f8071cbf20ee3dba2
xavierloos <xavier.loos04@gmail.com>
2021-07-11 12:53:22 +0100

    Update more user information

--- screens/ProfileScreen.js
+++ screens/ProfileScreen.js
@@ -6,6 +6,9 @@ import firebase from "firebase"
 
 const ProfileScreen = ({ navigation }) => {
   const [visible, setVisible] = useState(false)
+  const [newName, setNewName] = useState("")
+  const [newEmail, setNewEmail] = useState("")
+  const [newAvatar, setNewAvatar] = useState("")
   const [newPassword, setNewPassword] = useState("")
   const [currentPassword, setCurrentPassword] = useState("")
 
@@ -115,7 +118,9 @@ const ProfileScreen = ({ navigation }) => {
           <View style={styles.content}>
             <Avatar size={100} rounded source={{ uri: auth?.currentUser?.photoURL }} />
           </View>
-          <Input type="email" value={auth?.currentUser?.email} autoFocus autoCapitalize="none" leftIcon={<Icon name="at" type="font-awesome" size={30} color="#D50000" />} />
+          <Input type="text" placeholder={auth?.currentUser?.displayName} value={newName} autoFocus autoCapitalize leftIcon={<Icon name="user" type="font-awesome" size={30} color="#D50000" />}/>
+          <Input type="email" placeholder={auth?.currentUser?.email} value={newEmail} autoCapitalize="none" leftIcon={<Icon name="at" type="font-awesome" size={30} color="#D50000" />} keyboardType="email-address" />
+          <Input type="text" placeholder="Profile picture URL" value={newAvatar} leftIcon={<Icon name="photo" type="font-awesome" size={30} color="#D50000" style={{ marginRight: 10 }} />} />
           <Input secureTextEntry type="password" placeholder="Current Password" autoCapitalize="none" leftIcon={<Icon name="lock" type="font-awesome" size={30} color="#D50000" />} value={currentPassword} onChangeText={(text) => setCurrentPassword(text)} />
           <Input secureTextEntry type="password" placeholder="New Password" autoCapitalize="none" leftIcon={<Icon name="star" type="font-awesome" size={30} color="#D50000" />} value={newPassword} onChangeText={(text) => setNewPassword(text)} />
           <Button raised title="Update user" type="outline" onPress={onChangePasswordPress} />
