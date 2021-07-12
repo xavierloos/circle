de31edbccdd4bd88c5434315356ac4c3a0e33ded
xavierloos <xavier.loos04@gmail.com>
2021-07-12 20:27:43 +0100

    Update

--- App.js
+++ App.js
@@ -1,5 +1,4 @@
 import 'react-native-gesture-handler';
-// import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
