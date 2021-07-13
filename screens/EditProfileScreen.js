2cd5924c28f33969921a8f929be44fe62ad9af59
xavierloos <xavier.loos04@gmail.com>
2021-07-13 21:05:49 +0100

    Edit screen

---
+++ screens/EditProfileScreen.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+
+const EditProfileScreen = () => {
+  return (
+    <View>
+      <Text></Text>
+    </View>
+  )
+}
+
+export default EditProfileScreen
+
+const styles = StyleSheet.create({})
+
