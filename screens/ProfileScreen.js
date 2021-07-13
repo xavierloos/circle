ff1bd2e08fb85f27a6546af9fc15378bd538a412
xavierloos <xavier.loos04@gmail.com>
2021-07-13 19:51:25 +0100

    Update

--- screens/ProfileScreen.js
+++ screens/ProfileScreen.js
@@ -126,7 +126,6 @@ const ProfileScreen = ({ navigation }) => {
           <Input secureTextEntry type="password" placeholder="New Password" autoCapitalize="none" leftIcon={<Icon name="star" type="font-awesome" size={30} color="#D50000" />} value={newPassword} onChangeText={(text) => setNewPassword(text)} />
           <Button raised title="Update user" type="outline" onPress={onChangePasswordPress} />
         </View>
-        {closeBtn}
       </ModalPoup>
       <Button raised title="Logout" type="outline" containerStyle={styles.button} onPress={logout} />
     </View>
