bf7a782467ceed40b17a2f56080d0e5ed4a0cc80
xavierloos <xavier.loos04@gmail.com>
2021-07-13 21:06:55 +0100

    Edit profile screen stack

--- App.js
+++ App.js
@@ -9,6 +9,7 @@ import AddChatScreen from './screens/AddChatScreen';
 import ChatScreen from './screens/ChatScreen';
 import ChatInfoScreen from './screens/ChatInfoScreen';
 import ProfileScreen from './screens/ProfileScreen';
+import EditProfileScreen from './screens/EditProfileScreen';
 import { createStackNavigator } from "@react-navigation/stack";
 
 const Stack = createStackNavigator();
@@ -33,6 +34,7 @@ export default function App() {
         <Stack.Screen name="Chat" component={ChatScreen} />
         <Stack.Screen name="ChatInfo" component={ChatInfoScreen} />
         <Stack.Screen name="Profile" component={ProfileScreen} />
+        <Stack.Screen name="EditProfile" component={EditProfileScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
--- screens/EditProfileScreen.js
+++ screens/EditProfileScreen.js
@@ -3,7 +3,7 @@ import { StyleSheet, Text, View } from 'react-native'
 const EditProfileScreen = () => {
   return (
     <View>
-      <Text></Text>
+      <Text>Edit profile</Text>
     </View>
   )
 }
