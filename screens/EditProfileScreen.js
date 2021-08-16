9494ac592562ddf4dcda2344750039bf1e6d7991
xavierloos <xavier.loos04@gmail.com>
2021-08-16 16:27:30 +0100

    Update

--- screens/EditProfileScreen.js
+++ screens/EditProfileScreen.js
@@ -1,7 +1,8 @@
 import React, { useLayoutEffect } from 'react'
 import { StyleSheet, Text, View } from 'react-native'
 
-const EditProfileScreen = ({navigation}) => {
+const EditProfileScreen = ({ navigation }) => {
+  
   useLayoutEffect(() => {
     navigation.setOptions({
       headerTitle: "Edit Profile",
