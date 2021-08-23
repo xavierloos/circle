7d2727116ba533e1d6842303b240055bbea1cab5
xavierloos <xavier.loos04@gmail.com>
2021-08-23 16:31:16 +0100

    KeyboardAvoidingView

--- screens/EditProfileScreen.js
+++ screens/EditProfileScreen.js
@@ -38,20 +38,24 @@ const EditProfileScreen = ({ navigation }) => {
   }
 
   return (
-    <View style={styles.container}>
-      <Text>Edit profile</Text>
-      <View style={{ alignItems: "center" }}>
-        <View style={styles.content}>
-          <Avatar size={100} rounded source={{ uri: auth?.currentUser?.photoURL }} />
+    
+      <KeyboardAvoidingView behavior="padding" style={styles.container}>
+        <View style={styles.container}>
+          <Text>Edit profile</Text>
+          <View style={{ alignItems: "center" }}>
+            <View style={styles.content}>
+              <Avatar size={100} rounded source={{ uri: auth?.currentUser?.photoURL }} />
+            </View>
+            <Input type="text" placeholder={auth?.currentUser?.displayName} value={newName} autoFocus autoCapitalize="none" leftIcon={<Icon name="user" type="font-awesome" size={30} color="#D50000" />} />
+            <Input type="email" placeholder={auth?.currentUser?.email} value={newEmail} autoCapitalize="none" leftIcon={<Icon name="at" type="font-awesome" size={30} color="#D50000" />} keyboardType="email-address" />
+            <Input type="text" placeholder="Profile picture URL" value={newAvatar} leftIcon={<Icon name="photo" type="font-awesome" size={30} color="#D50000" style={{ marginRight: 10 }} />} />
+            <Input secureTextEntry type="password" placeholder="Current Password" autoCapitalize="none" leftIcon={<Icon name="lock" type="font-awesome" size={30} color="#D50000" />} value={currentPassword} onChangeText={(text) => setCurrentPassword(text)} />
+            <Input secureTextEntry type="password" placeholder="New Password" autoCapitalize="none" leftIcon={<Icon name="star" type="font-awesome" size={30} color="#D50000" />} value={newPassword} onChangeText={(text) => setNewPassword(text)} />
+            <Button raised title="Update user" type="outline" onPress={onChangePasswordPress} />
+          </View>
         </View>
-        <Input type="text" placeholder={auth?.currentUser?.displayName} value={newName} autoFocus autoCapitalize="none" leftIcon={<Icon name="user" type="font-awesome" size={30} color="#D50000" />} />
-        <Input type="email" placeholder={auth?.currentUser?.email} value={newEmail} autoCapitalize="none" leftIcon={<Icon name="at" type="font-awesome" size={30} color="#D50000" />} keyboardType="email-address" />
-        <Input type="text" placeholder="Profile picture URL" value={newAvatar} leftIcon={<Icon name="photo" type="font-awesome" size={30} color="#D50000" style={{ marginRight: 10 }} />} />
-        <Input secureTextEntry type="password" placeholder="Current Password" autoCapitalize="none" leftIcon={<Icon name="lock" type="font-awesome" size={30} color="#D50000" />} value={currentPassword} onChangeText={(text) => setCurrentPassword(text)} />
-        <Input secureTextEntry type="password" placeholder="New Password" autoCapitalize="none" leftIcon={<Icon name="star" type="font-awesome" size={30} color="#D50000" />} value={newPassword} onChangeText={(text) => setNewPassword(text)} />
-        <Button raised title="Update user" type="outline" onPress={onChangePasswordPress} />
-      </View>
-    </View>
+      </KeyboardAvoidingView>
+
   )
 }
 
