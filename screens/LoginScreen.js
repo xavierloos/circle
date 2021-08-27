416647664f60b01bb114737ec4c6ac8fa7240123
xavierloos <xavier.loos04@gmail.com>
2021-08-27 16:54:52 +0100

    Update

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -87,7 +87,7 @@ const styles = StyleSheet.create({
     height: "100%",
     display: "flex",
     flexDirection: "column",
-    backgroundColor: "transparent",
+    backgroundColor: "white",
   },
   formContainer: {
     alignItems: 'center',
