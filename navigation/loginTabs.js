a5e3edd98c3b16e0217a1f44df904b7d6f2f1546
xavierloos <xavier.loos04@gmail.com>
2021-08-31 14:18:33 +0100

    Import bottom-tabs

---
+++ navigation/loginTabs.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs"
+
+const loginTabs = () => {
+  return (
+    <View>
+      <Text></Text>
+    </View>
+  )
+}
+
+export default loginTabs
+
+const styles = StyleSheet.create({})
