f7608462b61eae7887da84e5d7cd5769ec5b03ed
xavierloos <xavier.loos04@gmail.com>
2021-08-31 14:19:48 +0100

    const Tab

--- navigation/loginTabs.js
+++ navigation/loginTabs.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs"
+import RegisterScreen from "../screens/RegisterScreen"
+
+const Tab = createBottomTabNavigator()
 
 const loginTabs = () => {
   return (
