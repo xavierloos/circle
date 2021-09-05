93dcca591954f382763a6e2b58150cc31b2fcbe1
xavierloos <xavier.loos04@gmail.com>
2021-09-05 22:19:11 +0100

    Update

--- screens/RegisterScreen.js
+++ screens/RegisterScreen.js
@@ -148,13 +148,12 @@ const styles = StyleSheet.create({
     backgroundColor: "transparent",
     top: -200
   },
-
   slogan: {
     fontWeight: "300",
     color: "#9A9A9A",
     paddingTop: 30
   },
-  
+
   span: {
     fontWeight: "800",
     color: "#D50000"
