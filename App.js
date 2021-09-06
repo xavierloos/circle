dca3062c6379900bff17390e83ed925bc2bc0b78
xavierloos <xavier.loos04@gmail.com>
2021-09-06 12:56:13 +0100

    Update

--- App.js
+++ App.js
@@ -41,11 +41,3 @@ export default function App() {
   );
 }
 
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: '#fff',
-//     alignItems: 'center',
-//     justifyContent: 'center',
-//   },
-// });
