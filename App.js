1fc34dd7b77261abe5b5aa343629dff2e8587a48
xavierloos <xavier.loos04@gmail.com>
2021-09-06 12:56:30 +0100

    Update

--- App.js
+++ App.js
@@ -1,6 +1,5 @@
 import 'react-native-gesture-handler';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import LoginScreen from './screens/LoginScreen';
 import RegisterScreen from './screens/RegisterScreen';
