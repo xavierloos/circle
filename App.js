092c5e0246dcdf30c678317245cb0eb9cd25571a
xavierloos <xavier.loos04@gmail.com>
2021-09-08 10:31:11 +0100

    Update

--- App.js
+++ App.js
@@ -28,7 +28,7 @@ export default function App() {
         screenOptions={globalScreenOptions}
       >
         <Stack.Screen name="Login" component={LoginScreen}/>
-        <Stack.Screen name="Register" component={RegisterScreen} />
+        <Stack.Screen name="Register" component={RegisterScreen}/>
         <Stack.Screen name="Home" component={HomeScreen}/>
         <Stack.Screen name="AddChat" component={AddChatScreen} />
         <Stack.Screen name="Chat" component={ChatScreen} />
