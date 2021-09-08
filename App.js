fe0cd5bb3fe6fc4453c68699c33d63f309e4230b
xavierloos <xavier.loos04@gmail.com>
2021-09-08 10:30:55 +0100

    update

--- App.js
+++ App.js
@@ -29,7 +29,7 @@ export default function App() {
       >
         <Stack.Screen name="Login" component={LoginScreen}/>
         <Stack.Screen name="Register" component={RegisterScreen} />
-        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Home" component={HomeScreen}/>
         <Stack.Screen name="AddChat" component={AddChatScreen} />
         <Stack.Screen name="Chat" component={ChatScreen} />
         <Stack.Screen name="ChatInfo" component={ChatInfoScreen} />
