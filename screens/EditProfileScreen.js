81c4805f6d9b353752ac17e4e6f0e34ddb0fb118
xavierloos <xavier.loos04@gmail.com>
2021-09-12 20:36:55 +0100

    Update

--- screens/EditProfileScreen.js
+++ screens/EditProfileScreen.js
@@ -96,7 +96,8 @@ const EditProfileScreen = ({ navigation }) => {
               value={currentPassword}
               onChangeText={(text) => setCurrentPassword(text)}
             />
-            <Input secureTextEntry type="password" placeholder="New Password" autoCapitalize="none" leftIcon={<Icon name="star" type="font-awesome" size={30} color="#D50000" />} value={newPassword} onChangeText={(text) => setNewPassword(text)} />
+            <Input
+              secureTextEntry type="password" placeholder="New Password" autoCapitalize="none" leftIcon={<Icon name="star" type="font-awesome" size={30} color="#D50000" />} value={newPassword} onChangeText={(text) => setNewPassword(text)} />
             <Button raised title="Update user" type="outline" onPress={onChangePasswordPress} />
           </View>
         </View>
