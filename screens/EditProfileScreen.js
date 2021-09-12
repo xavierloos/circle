6f7ea46773cbc65692e95ba3c618ef0279790467
xavierloos <xavier.loos04@gmail.com>
2021-09-12 20:46:23 +0100

    Update

--- screens/EditProfileScreen.js
+++ screens/EditProfileScreen.js
@@ -107,7 +107,8 @@ const EditProfileScreen = ({ navigation }) => {
                   type="font-awesome"
                   size={30} color="#D50000"
                 />
-              } value={newPassword} onChangeText={(text) => setNewPassword(text)} />
+              }
+              value={newPassword} onChangeText={(text) => setNewPassword(text)} />
             <Button raised title="Update user" type="outline" onPress={onChangePasswordPress} />
           </View>
         </View>
