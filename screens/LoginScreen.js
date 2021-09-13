0ffbcd9f076de5fc7b185f1bc24e90ee69486d35
xavierloos <xavier.loos04@gmail.com>
2021-09-13 12:51:56 +0100

    Update

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -84,7 +84,8 @@ const LoginScreen = ({ navigation }) => {
               />
               <Input
                 containerStyle={styles.input}
-                secureTextEntry type="password" autoCapitalize="none" placeholder="Password" value={password} onChangeText={(text) => setPassword(text)} onSubmitEditing={singIn} />
+                secureTextEntry
+                type="password" autoCapitalize="none" placeholder="Password" value={password} onChangeText={(text) => setPassword(text)} onSubmitEditing={singIn} />
             </View>
             <TouchableOpacity style={styles.button} onPress={singIn}>
               <Text style={styles.buttonText}>
